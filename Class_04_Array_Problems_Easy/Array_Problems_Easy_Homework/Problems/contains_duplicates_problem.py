545180d77eb387681728994327df74afb4b88684
Stephen8500 <73487095+Stephen8500@users.noreply.github.com>
2022-01-11 21:16:20 -0700

    Class 4 HW

--- Class_04_Array_Problems_Easy/Array_Problems_Easy_Homework/Problems/contains_duplicates_problem.py
+++ Class_04_Array_Problems_Easy/Array_Problems_Easy_Homework/Problems/contains_duplicates_problem.py
@@ -4,3 +4,24 @@
 
 input_array = [1, 2, 3, 3, 4]
 # Output = True
+# brute force duplicate finding
+def bruteForceDuplicates(input_array):
+    boolDuplicates = False
+    for i in input_array:
+        for j in input_array:
+            if i == j:
+                boolDuplicates = True
+
+    return boolDuplicates
+print(bruteForceDuplicates(input_array))
+
+# optimized solution:
+def duplicateCheck(input_array):
+    lenArray = len(input_array)
+    for i in range(lenArray):
+        test = input_array.pop(0)
+        if test in input_array:
+            return True
+    return False
+
+print(duplicateCheck(input_array))
