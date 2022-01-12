fe09869b0b6296f8389ef5423c595940886c4910
Stephen8500 <73487095+Stephen8500@users.noreply.github.com>
2022-01-11 21:16:36 -0700

    Class 4 pt 2

--- Class_04_Array_Problems_Easy/Array_Problems_Easy_Homework/Problems/best_time_to_buy_stock_problem.py
+++ Class_04_Array_Problems_Easy/Array_Problems_Easy_Homework/Problems/best_time_to_buy_stock_problem.py
@@ -5,3 +5,31 @@
 
 input_array = [7, 1, 5, 3, 6, 4]
 # Output = 5
+
+# brute force option to find best increase
+def bruteForceIncrease(input_array):
+    best_increase = 0
+
+    for i in range(len(input_array)):
+        for j in range(len(input_array)):
+            if (j > i) and input_array[j] - input_array[i] > best_increase:
+                best_increase = input_array[j] - input_array[i]
+
+    return best_increase
+
+print(bruteForceIncrease(input_array))
+
+# optimized findIncrease:
+def findBestIncrease(input_array):
+    size = 0
+    minNum = input_array[0]
+
+    for i in input_array:
+        if i < minNum:
+            minNum = i
+        if i - minNum > size:
+            size = i - minNum
+    
+    return size
+
+print(findBestIncrease(input_array))
