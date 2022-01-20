50c7fdd2e2e9da6b343ab1aff1303654da7460ef
Stephen8500 <73487095+Stephen8500@users.noreply.github.com>
2022-01-19 17:38:26 -0700

    inclass practice

---
+++ Class_05_Array_Problems_Medium/Array_Problems_Medium_Homework/InClass/inclass.py
@@ -0,0 +1,19 @@
+# array = [1,3,5,9,10,20,22,90,91]
+
+# def search(array, n):
+#     lower = 0
+#     upper = len(array) - 1
+
+#     while lower <= upper:
+#         mid = (lower + upper)//2
+
+#         if array[mid] == n:
+#             return mid
+#         elif array[mid] < n:
+#             lower = mid + 1
+#         else:
+#             upper = mid - 1
+#     return 'Not found'
+
+# print(search(array, 5))
+
